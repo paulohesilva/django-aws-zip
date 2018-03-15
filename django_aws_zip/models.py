6345182e8bf677ed31c9cbd5158713e52a60dc87
Lucas Macedo <lucas.macedo@hotmail.com>
2018-03-15 16:39:19 -0300

    Alter the size of the field "key" to 5000 character

--- django_aws_zip/models.py
+++ django_aws_zip/models.py
@@ -9,8 +9,8 @@ class Task(models.Model):
         (2, 'fail'),
     )
 
-    description = models.CharField(null=True, blank=True, max_length=100)
-    key = models.CharField(null=True, blank=True, max_length=100)
+    description = models.CharField(null=True, blank=True, max_length=1000)
+    key = models.CharField(null=True, blank=True, max_length=5000)
     num_file = models.IntegerField(null=True, blank=True)
     percent = models.IntegerField(null=True, blank=True)
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
